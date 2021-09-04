a895e0658983fa9f94716f82e01d86522064ba48
Steven Peñaloza <spenaloza1@udi.edu.co>
2021-09-04 16:22:27 -0500

    Conexión Terminada con Form
    Okay

--- FromPrac1/Form1.cs
+++ FromPrac1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ namespace FromPrac1
 {
     public partial class Form1 : Form
     {
+        public SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=SSPI;Initial Catalog=DBPrac1");
+        SqlCommand cmd;
+        SqlDataReader dr;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +24,29 @@ namespace FromPrac1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            con.Open();
+            MessageBox.Show("Conexion Creada Con Exito!");
+            con.Close();
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            string query = "Select * from APODERADO";
+            cmd = new SqlCommand(query, con);
+
+            SqlDataAdapter dAdapter = new SqlDataAdapter(cmd);
+
+            DataSet ds = new DataSet();
+
+            dAdapter.Fill(ds);
+
+            dataGridView1.ReadOnly = true;
+
+            dataGridView1.DataSource = ds.Tables[0];
+
+            con.Close();
         }
     }
 }
